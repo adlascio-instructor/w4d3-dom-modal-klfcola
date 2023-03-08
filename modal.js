76fc4d3b4e22e757494a38f9854a4be53968eb41
Kevin Lee <kevinlfcola@gmail.com>
2023-03-07 17:46:58 -0800

    review

--- modal.js
+++ modal.js
@@ -1,9 +1,27 @@
 console.log("here");
 
 // select button element
+const btn = document.querySelector("#openModal");
+const modalDiv = document.querySelector("#modal");
+console.log("btn", btn);
 
 // add event listener on click to button
-// remove class invisible from div modal
+btn.addEventListener("click", btnClicked);
+
+function btnClicked() {
+    //   console.log("click");
+    // remove class invisible from div modal
+    modalDiv.classList.remove("invisible");
+  }
 
 // add event listener on key press Escape
-// add class invisible to div modal
+document.body.addEventListener("keydown", leaveModal);
+
+function leaveModal(event) {
+    console.log("key", event.key);
+    if (event.key === "Escape") {
+      console.log("pressed Escape");
+      // add class invisible to div modal
+      modalDiv.classList.add("invisible");
+    }
+  }
